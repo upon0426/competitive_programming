6c7eec4cf96e03e6aafac190effdd0bab4c915f3
upon0426 <karasawa.yui@gmail.com>
2020-05-07 11:46:19 +0900

    161

--- AtCoder/ABC/161/d.cpp
+++ AtCoder/ABC/161/d.cpp
@@ -3,24 +3,31 @@ using namespace std;
  
 int main() {
 	int k;
-    cin >> k;
-    vector<long long> a;
-    for (int i=1;i<=9;i++) a.push_back(i);
-    while (1) {
-        if (k <= a.size()) {
-            cout << a[k-1] << endl;
-            return (0);
-        }
-        k -= a.size();
-        vector<long long> old;
-        swap(a,old);
-        for (long long x : old) {
-            for (int i=-1;i<=1;i++) {
-                int d = x % 10 + i;
-                if (d < 0 || 9 < d) continue;
-                long long nx = 10 * x + d;
-                a.push_back(nx);
-            }
-        }
-    }
+  	cin >> k;
+  	vector<long long> a;
+    // push_back使う時はi=0から入っていくのに注意
+  	for(int i=1;i<=9;i++) a.push_back(i);
+  	while (1) {
+      // 新しく作った配列aのサイズが、減っていくk以上になるようにしていく
+      if (k <= a.size()) {
+          cout << a[k-1] << endl;
+          // 出力したらプログラム終了
+    	  return (0);
+      }
+      // kの方が大きい場合は古いaのサイズ分減らす
+      k -= a.size();
+      vector<long long> old;
+      swap(a, old);
+      // 古いaをもとに新しいlunlun数を空の配列にいれていく
+      for (long long x : old) {
+          for(int i=-1;i<=1;i++) {
+              int d = x%10 + i;
+              if (d < 0 || 9 < d) continue;
+              long long nx = 10*x + d;
+              //cout << nx << endl;
+              a.push_back(nx);
+          }
+      }
+	}
+  
 }
