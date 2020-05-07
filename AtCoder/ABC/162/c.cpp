15e0e047e39a7979b7a32d96a30aae3377148521
upon0426 <karasawa.yui@gmail.com>
2020-05-07 10:06:38 +0900

    162c

--- AtCoder/ABC/162/c.cpp
+++ AtCoder/ABC/162/c.cpp
@@ -1,26 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int	gcd(int a, int b) {
-	if (b==0) return(a);
-  	return(gcd(b,a%b));
-}
-
-int main () {
-	int k;
-  	cin >> k;
-    int ans = 0;
-  	for (int i=1;i<=k;i++) {
-    	for (int j=1;j<=k;j++) {
-        	for (int l=1;l<=k;l++) {
-              int c;
-              if (i < j) c = gcd(j,i);
-              else c = gcd(i,j);
-			  if (c < l) ans += gcd(l,c);
-              else ans += gcd(c, l);
+int main() {
+  	int k;
+	cin >> k;
+  	int total = 0;
+  	for(int a=1;a<=k;a++) {
+    	for (int b=1;b<=k;b++) {
+        	for(int c=1;c<=k;c++) {
+            	total += gcd(gcd(a, b),c);
             }
         }
     }
-  cout << ans << endl;
-  	
+  	cout << total << endl;
 }
