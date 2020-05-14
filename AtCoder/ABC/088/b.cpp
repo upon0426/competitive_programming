98506c86ce62529aafb5cba0497e84cedcf03aed
upon0426 <karasawa.yui@gmail.com>
2020-05-14 22:43:01 +0900

    alice

--- AtCoder/ABC/088/b.cpp
+++ AtCoder/ABC/088/b.cpp
@@ -6,16 +6,18 @@ typedef long long ll;
 int main(void)
 {
     int n;
+    cin >> n;
     vector<int> a(n);
     rep (i, n) cin >> a[i];
-    sort(a.begin(), a.end(), greater<>());
-    rep(i, n) cout << a[i];
+    sort(a.begin(), a.end(), greater<int>());
+
     int alice, bob;
     alice = 0;
     bob = 0;
-    rep (i, n) {
+    rep (i, a.size()) {
         if (i%2==0) alice += a[i];
         else bob += a[i];
     }
     cout << alice - bob << endl;
+    
 }
