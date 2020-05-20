91fa11ee57276b163d5d564f358d6efaa4d3f305
upon0426 <karasawa.yui@gmail.com>
2020-05-20 23:31:10 +0900

    blackjack

---
+++ AtCoder/ABC/147/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+typedef long long ll;
+
+int main(void)
+{
+    int a1, a2, a3;
+    cin >> a1 >> a2 >> a3;
+    if (a1 + a2 + a3 >= 22) cout << "bust" << endl;
+    else  cout << "win" << endl;
+}
+
